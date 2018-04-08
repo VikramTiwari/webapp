bdc9de4fdfd36e6f0d72eb6f1f685ddd0080c024
Vikram Tiwari <vikramtheone1@gmail.com>
2018-04-07 17:32:46 -0700

    port as env variable with sane defaults

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const api = require('./backend/api');
 
-const PORT = 3000;
+const PORT = process.env['PORT'] || 3000;
 
 app.use('/api', api);
 app.use('/', express.static(path.join(__dirname, 'frontend/public')));
